0ab981a91bd800d5ff3cd1a4176f0df96a49a7c0
Annath Beevi <annathbeevi01@gmail.com>
2023-11-29 15:28:20 +0530

    JWT commit

---
+++ frontend/src/Components/Navigation.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { Outlet, Link } from 'react-router-dom';
+import { AppBar, Box, Tabs, Toolbar, Typography, Tab } from "@mui/material"
+import {useSelector} from 'react-redux'
+import { authActions} from '../Store/index'
+import { useDispatch } from 'react-redux';
+import axios from "axios"
+
+axios.defaults.withCredentials = true;
+
+const Navigation = () => {
+
+    const dispatch = useDispatch()
+
+    const isLoggedIn = useSelector((state) => state.isLoggedIn);
+    console.log(isLoggedIn)
+
+    const [value, setValue] = useState();
+
+    const onRequestSender = async () => {
+        const response = await axios.post("http://localhost:5000/logout", null, {
+            withCredentials: true,
+        })
+        if(response.status == 200) {
+            return response
+        }
+        return new Error("Something went wrong")
+    }
+
+    const logoutHandler = () => {
+        onRequestSender().then(() => dispatch(authActions.logout()))
+    }
+
+  return (
+    <div>
+        <AppBar>
+            <Toolbar>
+                <Typography variant='h4'>JWT</Typography>
+                <Box sx={{ marginLeft: "auto"}}>
+                    <Tabs 
+                    textColor='inherit'
+                    onChange={(e, val) => setValue(val)}
+                    value={value}>
+                        {!isLoggedIn && (
+                            <>
+                                <Tab label="Login" LinkComponent={Link} to="/login"/>
+                                <Tab label="Signup" LinkComponent={Link} to="/signup"/>
+                            </>
+                        )}
+                        { isLoggedIn && <Tab label="Logout" onClick={logoutHandler} LinkComponent={Link} to="/"/>}
+                    </Tabs>
+                </Box>
+            </Toolbar>
+        </AppBar>
+        <Outlet/>
+    </div>
+  )
+}
+
+export default Navigation
